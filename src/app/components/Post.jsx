b1601fa1acc948f909ff9651163561ebb138f130
Sanjit Ghosh <sanjitkumarghosh0@gmail.com>
2025-06-14 21:42:14 +0600

    add section title

--- src/app/components/Post.jsx
+++ src/app/components/Post.jsx
@@ -14,6 +14,8 @@ export default async function Post() {
   if (posts) {
     return (
       <div className='py-14'>
+        <h1 className='text-center font-medium text-3xl'>Recent Blogs</h1>
+        <div className='w-52 h-0.5 bg-[#4d6bfe] mx-auto my-3'></div>
         <div className='max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 p-5'>
           {posts.map((post) => (
             <div key={post._id}>
